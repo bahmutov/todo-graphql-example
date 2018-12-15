55be34042bd8e098187759abe17e5b9658d87e8d
Gleb Bahmutov <gleb.bahmutov@gmail.com>
2018-12-15 11:55:10 -0500

    testing loading

--- cypress/integration/spec.js
+++ cypress/integration/spec.js
@@ -3,3 +3,29 @@ it('loads 2 items', () => {
   cy.visit('/')
   cy.get('.todo-list li').should('have.length', 2)
 })
+
+it('shows loading message', () => {
+  cy.server()
+  cy.route({
+    method: 'POST',
+    url: 'http://localhost:3000/',
+    delay: 1000,
+    status: 200,
+    response: {
+      errors: [],
+      data: {
+        allTodos: []
+      }
+    }
+  })
+
+  cy.visit('/', {
+    onBeforeLoad (win) {
+      // force Apollo client to use XHR
+      delete win.fetch
+    }
+  })
+  cy.contains('.main', 'Loading...').should('be.visible')
+  // and then it should disappear from the DOM
+  cy.contains('.main', 'Loading...').should('not.exist')
+})
