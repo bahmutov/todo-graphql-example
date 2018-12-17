d8a910242aceadfed80d5d787865c8ad75d7d14d
Gleb Bahmutov <gleb.bahmutov@gmail.com>
2018-12-17 10:24:25 -0500

    add mutation to save new todo

--- src/containers/TodoTextInput.jsx
+++ src/containers/TodoTextInput.jsx
@@ -1,5 +1,15 @@
 import classnames from 'classnames'
+import gql from 'graphql-tag'
 import React, { Component } from 'react'
+import { Mutation } from 'react-apollo'
+
+const ADD_TODO = gql`
+  mutation AddTodo($id: ID!, $title: String!) {
+    createTodo(id: $id, title: $title) {
+      id
+    }
+  }
+`
 
 export default class TodoTextInput extends Component {
   // static propTypes = {
@@ -17,10 +27,17 @@ export default class TodoTextInput extends Component {
     }
   }
 
-  handleSubmit (e) {
+  handleSubmit (addTodo, e) {
     const text = e.target.value.trim()
     if (e.which === 13) {
-      this.props.onSave(text)
+      // this.props.onSave(text)
+      addTodo({
+        variables: {
+          id: 1,
+          title: text,
+          completed: false
+        }
+      })
       if (this.props.newTodo) {
         this.setState({ text: '' })
       }
@@ -39,19 +56,23 @@ export default class TodoTextInput extends Component {
 
   render () {
     return (
-      <input
-        className={classnames({
-          edit: this.props.editing,
-          'new-todo': this.props.newTodo
-        })}
-        type='text'
-        placeholder={this.props.placeholder}
-        autoFocus
-        value={this.state.text}
-        onBlur={this.handleBlur.bind(this)}
-        onChange={this.handleChange.bind(this)}
-        onKeyDown={this.handleSubmit.bind(this)}
-      />
+      <Mutation mutation={ADD_TODO}>
+        {(addTodo, { data }) => (
+          <input
+            className={classnames({
+              edit: this.props.editing,
+              'new-todo': this.props.newTodo
+            })}
+            type='text'
+            placeholder={this.props.placeholder}
+            autoFocus
+            value={this.state.text}
+            onBlur={this.handleBlur.bind(this)}
+            onChange={this.handleChange.bind(this)}
+            onKeyDown={this.handleSubmit.bind(this, addTodo)}
+          />
+        )}
+      </Mutation>
     )
   }
 }
