f99c0fe41772cde14b5deb068c3670b55f413ead
Gleb Bahmutov <gleb.bahmutov@gmail.com>
2020-10-01 21:43:10 -0400

    more testing

--- cypress/integration/route2-spec.js
+++ cypress/integration/route2-spec.js
@@ -65,9 +65,49 @@ describe('TodoMVC with GraphQL', () => {
     cy.visit('/')
     cy.wait('@allTodos')
     cy.get('.todo-list li').should('have.length', 2)
+
     // new todos should be added
     const randomTitle = `New todo ${Cypress._.random(1e10)}`
     cy.get('.new-todo').type(`${randomTitle}{enter}`)
     cy.contains('.todo-list li', randomTitle)
+  })
+
+  it('spies on adding todos', () => {
+    let todoResponse
+
+    cy.route2(
+      {
+        method: 'POST',
+        url: '/',
+      },
+      (req) => {
+        const body = JSON.parse(req.body)
+        if (body.operationName === 'AddTodo') {
+          req.reply((res) => {
+            todoResponse = JSON.parse(res.body).data
+            console.log('new todo')
+            console.log(todoResponse)
+          })
+        }
+      },
+    )
+
+    cy.visit('/')
+
+    // new todos should be added
+    const randomTitle = `New todo ${Cypress._.random(1e10)}`
+    cy.get('.new-todo').type(`${randomTitle}{enter}`)
+
+    cy.contains('.todo-list li', randomTitle)
+      .should(() => {
+        expect(todoResponse)
+          .to.be.an('object')
+          .and.to.have.property('createTodo')
+      })
+      .then(() => {
+        cy.wrap(todoResponse.createTodo).as('todo')
+      })
+
+    cy.get('@todo').should('have.property', 'id')
   })
 })
