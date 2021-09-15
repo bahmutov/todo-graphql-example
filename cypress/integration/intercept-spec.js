e37c92540f7b56ed17e4b31f595a549ed089d507
Gleb Bahmutov <gleb.bahmutov@gmail.com>
2021-09-15 10:57:00 -0400

    add graphql operation name header (#98)
    * add graphql operation name header
    
    * add test showing custom header
    
    * more checks

--- cypress/integration/intercept-spec.js
+++ cypress/integration/intercept-spec.js
@@ -257,4 +257,57 @@ describe('TodoMVC with GraphQL cy.intercept', () => {
 
     cy.get('@todo').should('have.property', 'id')
   })
+
+  it('intercepts operations using custom header', () => {
+    // we have special middleware in our GraphQL client
+    // that puts the operation name in the request header "x-gql-operation-name"
+    // we can define intercepts using this custom header
+    cy.intercept({
+      method: 'POST',
+      url: '/',
+      headers: {
+        'x-gql-operation-name': 'allTodos',
+      },
+    }).as('allTodos')
+
+    cy.intercept({
+      method: 'POST',
+      url: '/',
+      headers: {
+        'x-gql-operation-name': 'AddTodo',
+      },
+    }).as('addTodo')
+
+    cy.intercept({
+      method: 'POST',
+      url: '/',
+      headers: {
+        'x-gql-operation-name': 'updateTodo',
+      },
+    }).as('updateTodo')
+
+    cy.visit('/')
+    cy.wait('@allTodos')
+
+    cy.get('.new-todo').type('add an item{enter}')
+    cy.wait('@addTodo')
+      // let's confirm the item the app is sending
+      .its('request.body.variables')
+      .should('deep.equal', {
+        title: 'add an item',
+      })
+    // the server responds with the id of the new item
+    // since we have waited for the request to happen
+    // now we can just use cy.get(alias) to retrieve it again
+    cy.get('@addTodo')
+      .its('response.body.data.createTodo')
+      .should('have.property', 'id')
+
+    // after adding a todo, the app fetches the list again
+    cy.wait('@allTodos')
+
+    // let's complete the last item
+    cy.get('.todo').last().find('.toggle').click()
+    cy.wait('@updateTodo')
+  })
 })
