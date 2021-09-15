bf784f1a96716b09d65f95c71e7cb1010aa5c9c4
Gleb Bahmutov <gleb.bahmutov@gmail.com>
2021-09-15 09:18:33 -0400

    add spy assertion

--- cypress/integration/ui-spec.js
+++ cypress/integration/ui-spec.js
@@ -2,6 +2,9 @@
 
 describe('TodoMVC', () => {
   it('toggles todo', () => {
+    // this test looks at the page UI
+    // to determine how the class changes
+    // when we toggle a todo
     let startClass
     cy.visit('/')
     cy.get('li.todo')
@@ -20,6 +23,32 @@ describe('TodoMVC', () => {
       .invoke('attr', 'class')
       .should((x) => {
         expect(x).to.not.equal(startClass)
+      })
+  })
+
+  it('toggles todo after observing network calls', () => {
+    // this test spies on the initial network call
+    // to see the first todo item and if it is completed or not
+    cy.intercept('POST', '/').as('allTodos')
+    cy.visit('/')
+    cy.wait('@allTodos')
+      .its('response.body.data.allTodos.0.completed')
+      .then((completed) => {
+        // from the network response
+        // we can form the right assertion
+        const classAssertion = completed ? 'have.class' : 'not.have.class'
+        cy.get('.todo')
+          .should('have.length', 2)
+          .first()
+          .should(classAssertion, 'completed')
+          .find('.toggle')
+          .click()
+
+        const toggleAssertion = completed ? 'not.have.class' : 'have.class'
+        cy.get('.todo')
+          .should('have.length', 2)
+          .first()
+          .should(toggleAssertion, 'completed')
       })
   })
 })
