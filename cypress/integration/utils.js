fa061badaf789c5b3637c873095b54031fc9528d
Gleb Bahmutov <gleb.bahmutov@gmail.com>
2021-09-27 11:36:24 -0400

    add dynamic test example

---
+++ cypress/integration/utils.js
@@ -0,0 +1,51 @@
+/// <reference types="cypress" />
+
+import { gql } from '@apollo/client'
+import { client } from '../../src/graphql-client'
+
+export function deleteAll() {
+  // fetches all todo items, grabs their IDs, and deletes them
+  cy.log('**deleteAll**')
+    .then(() =>
+      client.query({
+        // it is important to AVOID any caching here
+        // and fetch the current server data
+        fetchPolicy: 'no-cache',
+        query: gql`
+          query getAllTodos {
+            allTodos {
+              id
+            }
+          }
+        `,
+      }),
+    )
+    .its('data.allTodos')
+    // from each item, grab just the property "id"
+    .then((items) => Cypress._.map(items, 'id'))
+    .then((ids) => {
+      if (!ids.length) {
+        cy.log('Nothing to delete')
+        return
+      }
+      cy.log(`Found **${ids.length}** todos`)
+
+      // delete all items one by one
+      ids.forEach((id) => {
+        const mutation = gql`
+            mutation deleteTodo {
+              removeTodo(id: "${id}") {
+                id
+              }
+            }
+          `
+        cy.log(`deleting item id:**${id}**`).then(
+          () =>
+            client.mutate({
+              mutation,
+            }),
+          { log: false },
+        )
+      })
+    })
+}
