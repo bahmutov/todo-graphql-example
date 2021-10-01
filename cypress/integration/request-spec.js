671c842d7a89f7f7848db232139aa5e3003cb8b6
Gleb Bahmutov <gleb.bahmutov@gmail.com>
2021-10-01 11:56:12 -0400

    add more tests

--- cypress/integration/request-spec.js
+++ cypress/integration/request-spec.js
@@ -4,7 +4,91 @@ import { gql } from '@apollo/client'
 import { client } from '../../src/graphql-client'
 
 describe('Make GraphQL requests', () => {
-  it('creates an item', () => {
+  it('fetches all items', () => {
+    cy.request({
+      method: 'POST',
+      url: 'http://localhost:3000/',
+      body: {
+        operationName: 'allTodos',
+        query: `
+          query allTodos {
+            allTodos {
+              id,
+              title,
+              completed
+            }
+          }
+        `,
+      },
+    })
+      .its('body.data.allTodos')
+      .should('have.length', 2)
+  })
+
+  it('fetches all items using application client', () => {
+    // make a GraphQL query using the app's client
+    // https://www.apollographql.com/docs/react/data/queries/
+    const query = gql`
+      query allTodos {
+        allTodos {
+          id
+          title
+          completed
+        }
+      }
+    `
+
+    // use https://on.cypress.io/wrap to let the Cypress test
+    // wait for the promise returned by the "client.query" to resolve
+    cy.wrap(
+      client.query({
+        query,
+        // it is important to AVOID any caching here
+        // and fetch the current server data
+        fetchPolicy: 'no-cache',
+      }),
+    )
+      .its('data.allTodos')
+      .should('have.length', 2)
+  })
+
+  it('creates one item', () => {
+    const random = Cypress._.random(1e5)
+    const title = `test ${random}`
+    cy.log(`Adding item ${title}`)
+      .then(() => {
+        const query = gql`
+          mutation AddTodo($title: String!) {
+            createTodo(title: $title, completed: false) {
+              id
+            }
+          }
+        `
+
+        // by returning the promise returned by the "client.query"
+        // call from the .then callback, we force the test to wait
+        // and yield the result to the next Cypress command or assertion
+        return client.query({
+          query,
+          variables: {
+            title,
+          },
+          // it is important to AVOID any caching here
+          // and fetch the current server data
+          fetchPolicy: 'no-cache',
+        })
+      })
+      // use zero timeout to avoid "cy.its" retrying
+      // since the response object is NOT going to change
+      .its('data.createTodo', { timeout: 0 })
+      .should('have.property', 'id')
+
+    // the item we have created should be shown in the list
+    cy.visit('/')
+    cy.contains('.todo', title)
+  })
+
+  it('creates an item and fetches it', () => {
     cy.visit('/').wait(1000)
     const random = Cypress._.random(1e5)
     const title = `test ${random}`
