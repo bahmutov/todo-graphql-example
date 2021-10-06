d184a1d7dd43d9233d634a5295f38a1adee9a9b7
Gleb Bahmutov <gleb.bahmutov@gmail.com>
2021-10-06 13:19:02 -0400

    add getTodos utility

--- cypress/integration/utils.js
+++ cypress/integration/utils.js
@@ -4,13 +4,15 @@ import { gql } from '@apollo/client'
 import { client } from '../../src/graphql-client'
 
 /**
- * Deletes all items from the server
+ * Fetches all todos from the server
  */
-export function deleteAll() {
-  // fetches all todo items, grabs their IDs, and deletes them
-  cy.log('**deleteAll**')
-    .then(() =>
-      client.query({
+export function getTodos() {
+  // use a "dummy" cy.then(() => ...)
+  // to make the command "wait" its turn
+  // when executing in the Cypress command chain
+  return cy
+    .then(() => {
+      return client.query({
         // it is important to AVOID any caching here
         // and fetch the current server data
         fetchPolicy: 'no-cache',
@@ -21,9 +23,18 @@ export function deleteAll() {
             }
           }
         `,
-      }),
-    )
+      })
+    })
     .its('data.allTodos')
+}
+
+/**
+ * Deletes all items from the server
+ */
+export function deleteAll() {
+  // fetches all todo items, grabs their IDs, and deletes them
+  cy.log('**deleteAll**')
+  getTodos()
     // from each item, grab just the property "id"
     .then((items) => Cypress._.map(items, 'id'))
     .then((ids) => {
