9ed36a7a6d0a8171eb5567187983ba00501a396c
Gleb Bahmutov <gleb.bahmutov@gmail.com>
2021-10-06 13:53:45 -0400

    use cypress-data-session

---
+++ cypress/integration/completes-todo-spec.js
@@ -0,0 +1,43 @@
+// @ts-check
+
+import 'cypress-data-session'
+import { data } from '../fixtures/three.json'
+import { deleteAll, createItems, getTodos } from './utils'
+
+describe('completes todo', () => {
+  beforeEach(() => {
+    // https://github.com/bahmutov/cypress-data-session
+    cy.dataSession({
+      name: '3 todos',
+      preSetup: deleteAll,
+      setup() {
+        cy.log('**create 3 items**')
+        createItems(data.allTodos)
+        cy.wrap(Cypress._.map(data.allTodos, 'title'))
+      },
+      validate(titles) {
+        return getTodos().then((todos) => {
+          return Cypress._.isEqual(titles, Cypress._.map(todos, 'title'))
+        })
+      },
+    })
+  })
+
+  it('shows 3 todos', () => {
+    cy.visit('/')
+    cy.get('.todo').should('have.length', data.allTodos.length)
+  })
+
+  it('completes a todo', () => {
+    cy.visit('/')
+    cy.get('.todo')
+      .should('have.length', data.allTodos.length)
+      .eq(1)
+      .find('.toggle')
+      .click()
+      .then(() => {
+        Cypress.clearDataSession('3 todos')
+      })
+    cy.get('.todo').eq(1).should('have.class', 'completed')
+  })
+})
--- cypress/integration/utils.js
+++ cypress/integration/utils.js
@@ -19,6 +19,7 @@ export function getTodos() {
         query: gql`
           query getAllTodos {
             allTodos {
+              title
               id
             }
           }
