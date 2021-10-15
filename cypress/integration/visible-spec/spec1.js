0fda1f3154715860ded0ed5dfd5e9e3650cb7288
Gleb Bahmutov <gleb.bahmutov@gmail.com>
2021-10-15 10:20:22 -0400

    split into thirds

--- cypress/integration/visible-spec/spec1.js
+++ cypress/integration/visible-spec/spec1.js
@@ -7,6 +7,8 @@ import selectors from './selectors.json'
 import { testTitle, testElementSelector } from './utils'
 
 describe('visible elements', () => {
-  const filteredSelectors = selectors.filter((x, k) => k % 2 === 0)
+  // there are 3 spec files testing the "selectors" list
+  // this spec file will pick the selectors 0, 3, 6, etc.
+  const filteredSelectors = selectors.filter((x, k) => k % 3 === 0)
   it.each(filteredSelectors)(testTitle, testElementSelector)
 })
---
+++ cypress/integration/visible-spec/spec3.js
@@ -0,0 +1,12 @@
+// @ts-check
+
+// adds "describe.each" and "it.each" helpers
+// https://github.com/bahmutov/cypress-each
+import 'cypress-each'
+import selectors from './selectors.json'
+import { testTitle, testElementSelector } from './utils'
+
+describe('visible elements', () => {
+  const filteredSelectors = selectors.filter((x, k) => k % 3 === 2)
+  it.each(filteredSelectors)(testTitle, testElementSelector)
+})
