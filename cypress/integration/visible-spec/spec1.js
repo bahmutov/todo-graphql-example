33096598ee3062caa63a6ae3bc538a5d8d3dbbd9
Gleb Bahmutov <gleb.bahmutov@gmail.com>
2021-10-28 09:26:45 -0400

    add dynamic tag job

--- cypress/integration/visible-spec/spec1.js
+++ cypress/integration/visible-spec/spec1.js
@@ -3,7 +3,7 @@
 import selectors from './selectors.json'
 import { testTitle, testElementSelector } from './utils'
 
-describe('visible elements', () => {
+describe('visible elements', { tags: '@visible' }, () => {
   // there are 3 spec files testing the "selectors" list
   // this spec file will pick the selectors 0, 3, 6, etc.
   const filteredSelectors = selectors.filter((x, k) => k % 3 === 0)
