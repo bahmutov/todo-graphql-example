937ca98dc54fab7875a5be958a366a17fa66ddbd
Gleb Bahmutov <gleb.bahmutov@gmail.com>
2021-11-01 14:15:58 -0400

    use cypress-highlight plugin

--- cypress/integration/highlight-spec.js
+++ cypress/integration/highlight-spec.js
@@ -1,40 +1,16 @@
 // @ts-check
 import { deleteAll, createItems } from './utils'
-
-// adding a red outline around every element with
-// selector "data-cy"
-function highlightSelectors() {
-  cy.wrap(null, { log: false }).then(() => {
-    // @ts-ignore
-    const doc = cy.state('document')
-    const head = doc.head
-    const hasStyle = Cypress._.find(head.styleSheets, { title: 'highlight' })
-    if (hasStyle) {
-      return
-    }
-
-    const style = doc.createElement('style')
-    style.title = 'highlight'
-    style.type = 'text/css'
-    style.appendChild(
-      document.createTextNode(`
-        [data-cy] {
-          outline: 1px solid red !important;
-        }
-      `),
-    )
-    head.appendChild(style)
-  })
-}
+import { highlight } from 'cypress-highlight'
+const { range, random, sample } = Cypress._
 
 describe('Highlight', () => {
   beforeEach(() => {
     // make sure we start with several todos
     deleteAll()
-    const randomTodos = Cypress._.range(0, Cypress._.random(2, 4)).map((k) => {
+    const randomTodos = range(0, random(2, 4)).map((k) => {
       return {
         title: `Todo ${k}`,
-        completed: Cypress._.sample([true, false]),
+        completed: sample([true, false]),
       }
     })
     cy.log(`creating ${randomTodos.length} todos`)
@@ -44,10 +20,7 @@ describe('Highlight', () => {
   it('highlights elements with good selectors', () => {
     cy.visit('/')
     cy.get('li.todo').should('have.length.gt', 0)
-    highlightSelectors()
-    // the header should have the red outline
-    // NOTE: not the best way to test this, but it works
-    cy.get('header').should('have.css', 'outline')
-    cy.screenshot({ capture: 'runner' })
+    highlight('[data-cy]', '.todo')
+    cy.screenshot('selectors', { capture: 'runner' })
   })
 })
