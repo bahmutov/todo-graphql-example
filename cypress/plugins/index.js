0e639a4c961d3f1a8aa2f8d242daaf4c415ad4c4
Gleb Bahmutov <gleb.bahmutov@gmail.com>
2021-12-09 12:21:20 -0500

    add terminal timestamps

--- cypress/plugins/index.js
+++ cypress/plugins/index.js
@@ -2,6 +2,24 @@ module.exports = (on, config) => {
   // optional: register cypress-grep plugin code
   // https://github.com/bahmutov/cypress-grep
   require('cypress-grep/src/plugin')(config)
+
+  function getUTC() {
+    const now = new Date()
+    return now.toISOString()
+  }
+
+  on('task', {
+    beforeTest(testName) {
+      console.log(`=== ${getUTC()} start: ${testName}`)
+      // cy.task must return something, cannot return undefined
+      return null
+    },
+    afterTest(testName) {
+      console.log(`=== ${getUTC()} end: ${testName}`)
+      return null
+    },
+  })
+
   // make sure to return the config object
   // as it might have been modified by the plugin
   return config
--- cypress/support/index.js
+++ cypress/support/index.js
@@ -4,3 +4,11 @@ require('cypress-each')
 
 // https://github.com/bahmutov/cypress-grep
 require('cypress-grep')()
+
+beforeEach(() => {
+  cy.task('beforeTest', Cypress.currentTest.title)
+})
+
+afterEach(() => {
+  cy.task('afterTest', Cypress.currentTest.title)
+})
